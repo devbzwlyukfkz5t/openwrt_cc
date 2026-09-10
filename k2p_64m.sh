a71a132929bd824dca86ccf314cd064d54d95844
devbzwlyukfkz5t <103549158+devbzwlyukfkz5t@users.noreply.github.com>
2026-09-10 12:44:34 +0800

    Update k2p_64m.sh

--- k2p_64m.sh
+++ k2p_64m.sh
@@ -3,7 +3,7 @@
 
 # 64M 固件, https://www.bgegao.com/2020/11/1885.html
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x3f40000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x3fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/"Phicomm K2P";/"Phicomm K2P (64M)";/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/15744k/62000k/g' target/linux/ramips/image/mt7621.mk
 
